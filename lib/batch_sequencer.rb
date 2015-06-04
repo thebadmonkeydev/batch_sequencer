d434637a8eee0e199f78770e27d8fbd4f443069c
Michael Kelly <michaelkelly322@gmail.com>
2015-06-03 23:50:20 -0400

    Creates initial method entry point method

--- lib/batch_sequencer.rb
+++ lib/batch_sequencer.rb
@@ -1,5 +1,14 @@
 require "batch_sequencer/version"
 
 module BatchSequencer
-  # Your code goes here...
+  def self.run(batch_def, *args)
+    raise SequenceDefinitionError unless batch_def.is_a? Hash
+  end
+
+  class SequenceDefinitionError < ArgumentError
+    def self.message
+      'invalid batch definition'
+    end
+  end
 end
+
--- spec/batch_sequencer_spec.rb
+++ spec/batch_sequencer_spec.rb
@@ -5,4 +5,18 @@ describe BatchSequencer do
     expect(BatchSequencer::VERSION).not_to be nil
   end
 
+  it 'has a run method' do
+    expect(BatchSequencer).to respond_to :run
+  end
+
+  context '#run' do
+    it 'will only accept a hash as the first parameter' do
+      expect{BatchSequencer.run([])}.to raise_error BatchSequencer::SequenceDefinitionError
+    end
+
+    it 'can accept any number of parameters after the sequence definition' do
+      expect{BatchSequencer.run({}, 1)}.to_not raise_error
+    end
+  end
+
 end
