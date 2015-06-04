0b43634dc2ed7390dd33b59f911e52a94036edb5
Michael Kelly <michaelkelly322@gmail.com>
2015-06-04 15:35:09 -0400

    Refactor specs into module for brevity

--- spec/batch_sequencer_spec.rb
+++ spec/batch_sequencer_spec.rb
@@ -1,31 +1,33 @@
 require 'spec_helper'
 
-describe BatchSequencer do
-  let(:good_def) do
-    {
-      id: 1234,
-      class: 'Account',
-      jobs: []
-    }
-  end
-
-  it 'has a version number' do
-    expect(BatchSequencer::VERSION).not_to be nil
-  end
-
-  context 'gem API methods' do
-    it 'has a run method' do
-      expect(BatchSequencer).to respond_to :run
+module BatchSequencer
+  describe BatchSequencer do
+    let(:good_def) do
+      {
+        id: 1234,
+        class: 'Account',
+        jobs: []
+      }
     end
 
-    describe '#run' do
-      it 'ensures validation of sequence definition' do
-        expect_any_instance_of(BatchSequencer::SequenceDefinitionValidator).to receive :validate
-        BatchSequencer.run(good_def)
+    it 'has a version number' do
+      expect(BatchSequencer::VERSION).not_to be nil
+    end
+
+    context 'gem API methods' do
+      it 'has a run method' do
+        expect(BatchSequencer).to respond_to :run
       end
 
-      it 'can accept any number of parameters after the sequence definition' do
-        expect{BatchSequencer.run(good_def, 1)}.to_not raise_error
+      describe '#run' do
+        it 'ensures validation of sequence definition' do
+          expect_any_instance_of(SequenceDefinitionValidator).to receive :validate
+          BatchSequencer.run(good_def)
+        end
+
+        it 'can accept any number of parameters after the sequence definition' do
+          expect{BatchSequencer.run(good_def, 1)}.to_not raise_error
+        end
       end
     end
   end
--- spec/sequence_definition_validator_spec.rb
+++ spec/sequence_definition_validator_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
-describe BatchSequencer::SequenceDefinitionValidator do
-  context '#validate' do
-    it 'throws an error when the definition it not a hash' do
-      expect{BatchSequencer::SequenceDefinitionValidator[[]].validate}.to raise_error 'sequence definition is not a hash'
+module BatchSequencer
+  describe SequenceDefinitionValidator do
+    context '#validate' do
+      it 'throws an error when the definition it not a hash' do
+        expect{SequenceDefinitionValidator[[]].validate}.to raise_error 'sequence definition is not a hash'
+      end
     end
   end
 end
